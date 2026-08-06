242b0e28f4838c3f83c8d5fa4f83a29220d32fdb
AyushGU12 <ayushgu12@github.com>
2026-08-06 10:50:57 +0530

    add: http middleware converting HEAD requests to GET globally for UptimeRobot free tier

--- src/api/main.py
+++ src/api/main.py
@@ -36,9 +36,21 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
+
+from starlette.requests import Request
+from starlette.responses import Response
+
+@app.middleware("http")
+async def head_to_get_middleware(request: Request, call_next):
+    if request.method == "HEAD":
+        request.scope["method"] = "GET"
+        response = await call_next(request)
+        return Response(status_code=response.status_code, headers=dict(response.headers))
+    return await call_next(request)
 
 from fastapi.responses import FileResponse
 
